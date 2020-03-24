c1897b2ef1819a96996b1371ce8bd20d8a5126af
antoine.ducellier <antoine.ducellier@epitech.eu>
2020-03-24 11:14:47 +0100

    start

---
+++ Bootstrap_Ducellier/leminguay.h
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2019
+** leminguay.h
+** File description:
+** ok
+*/
+
+#ifndef LEMINGUAY_H
+#define LEMINGUAY_H
+
+#include <stdlib.h>
+#include <unistd.h>
+
+typedef struct link
+{
+    int data ;
+    struct link * next ;
+} link_t ;
+
+
+#endif
